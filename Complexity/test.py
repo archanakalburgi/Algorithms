80c2b41c504f57afecc8cadd84d2840a3c7135bf
Archana Kalburgi <archanakalburgi@gmail.com>
2020-08-14 21:39:07 +0530

    Problems on recursion and Insertion sort

--- Complexity/test.py
+++ Complexity/test.py
@@ -1,16 +1,12 @@
-def pascal_triagle(rows):
-    l = [1]
-    for i in range(rows):
-        print(l)
-        newlist = []
-        newlist.append(l[0])
+def insertion_sort(A):
+    for j in range(2,len(A)):
+        key = A[j]
+        i = j-1
+        while i>0 and A[i]>key :
+            A[i+1] = A[i]
+            i = i-1 
+        A[i+1] = key
+    return A  
 
-        for i in range(len(l)-1):
-            newlist.append(l[i] + l[i+1])
-
-        newlist.append(l[-1])
-        l = newlist  
-
-pascal_triagle(4)
-
-#no of steps = 46
+array = [6,3,9,1,2,0]
+print(insertion_sort(array)) 
