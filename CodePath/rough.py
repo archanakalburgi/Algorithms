f7e19d542a5921ef8faff0edb3d59f7944ef4691
Archana Kalburgi <archanakalburgi@Archanas-MBP.lan>
2021-07-22 10:07:50 -0400

    Binary Search Template II

--- CodePath/rough.py
+++ CodePath/rough.py
@@ -1,7 +1,1 @@
-def insert(head, val, ListNode):
-    temp = head
-    while temp.val != val:
-        temp = temp.next
-    temp2 = temp.next
-    temp.next = ListNode
-    ListNode.next = temp2 
+"""Binary search templates"""
