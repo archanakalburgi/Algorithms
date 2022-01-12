ab8682705cb3b283c0c60a0b526e37b9d3b8eef2
Archana Kalburgi <archanakalburgi@Archanas-MBP.lan>
2022-01-12 07:35:19 -0500

    adding new unsolved problems

---
+++ practice_problems/resultword_searchword.py
@@ -0,0 +1,11 @@
+"""
+input
+    searchword = "armaz"
+    resultword = "amazon"
+
+output
+    2
+
+explaination
+    if you add "on" to "armaz" which becomes "armazon"  which is a bigger set of resultword = "amazon"
+"""
